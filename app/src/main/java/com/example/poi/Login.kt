bd8060c335d2ab952d53266b4f8f23d7850c663f
Comicnist099 <marco.ctu@hotmail.com>
2022-03-03 03:38:07 -0600

    Inicio de Sesion

--- app/src/main/java/com/example/poi/Login.kt
+++ app/src/main/java/com/example/poi/Login.kt
@@ -1,11 +1,55 @@
 package com.example.poi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
+
+
+    private lateinit var edtEmail:EditText
+    private lateinit var edtPassword:EditText
+    private lateinit var btnLogin:Button
+    private lateinit var btnSignUp:Button
+
+    private lateinit var mAuth:FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+
+        mAuth= FirebaseAuth.getInstance()
+
+        edtEmail= findViewById(R.id.edt_email)
+        edtPassword= findViewById(R.id.edt_password)
+        btnLogin= findViewById(R.id.bntLogin)
+        btnSignUp= findViewById(R.id.bntSignUp)
+
+        btnSignUp.setOnClickListener{
+            val intent=Intent(this,SignUp::class.java)
+
+            startActivity(intent)
+
+        }
+
+
+        btnLogin.setOnClickListener {
+            val email=edtEmail.text.toString()
+            val password=edtPassword.text.toString()
+
+            login(email,password)
+
+
+        }
+    }
+    private fun login(email:String,password: String){
+
     }
 }
