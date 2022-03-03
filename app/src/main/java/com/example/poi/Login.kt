11be13a660edd21e81c144b3a0e26a755578e60a
Comicnist099 <marco.ctu@hotmail.com>
2022-03-03 04:25:31 -0600

    Mañana le sigo ajaj

--- app/src/main/java/com/example/poi/Login.kt
+++ app/src/main/java/com/example/poi/Login.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import com.google.firebase.ktx.Firebase
@@ -50,6 +51,14 @@ class Login : AppCompatActivity() {
         }
     }
     private fun login(email:String,password: String){
-
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent=Intent(this@Login, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@Login, "El usuario no existe", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
