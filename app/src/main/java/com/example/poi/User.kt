b22a143eb98ed25cf7e24ae07d9fbed025d12dc8
Comicnist099 <marco.ctu@hotmail.com>
2022-03-04 02:22:56 -0600

    RealTime

--- app/src/main/java/com/example/poi/User.kt
+++ app/src/main/java/com/example/poi/User.kt
@@ -8,10 +8,11 @@ class User {
 
     constructor(){}
 
-    constructor(name: String?, email: String?, uid:String?)
+    constructor(name: String?, email: String?, uid:String?) {
 
-    this.name = name
-    this.email = email
-    this.uid = uid
+        this.name = name
+        this.email = email
+        this.uid = uid
+    }
 
 }
